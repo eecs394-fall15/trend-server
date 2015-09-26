6cb9870cf33307c7ed67f4ac85e5da9fdda2da8b
Adel Lahlou <adellahlou@users.noreply.github.com>
2015-09-26 17:05:57 -0500

    Added lists, restructured twitter code

--- routes/twitterRoutes.js
+++ routes/twitterRoutes.js
@@ -1,10 +1,9 @@
+//dependencies
 var express = require('express');
 var router = express.Router();
 var Twitter = require('twitter');
 
-
-
-
+//authenticate Twitter
 var client = new Twitter({
 	consumer_key: process.env.CONSUMER_KEY,
 	consumer_secret: process.env.CONSUMER_SECRET,
@@ -12,46 +11,63 @@ var client = new Twitter({
 	access_token_secret: process.env.ACCESS_TOKEN_SECRET
 });
 
-console.log('consumer_key: ', process.env.CONSUMER_KEY,
-	'consumer_secret ', process.env.CONSUMER_SECRET,
-	'access_token_key ', process.env.ACCESS_TOKEN_KEY,
-	'access_token_secret ', process.env.ACCESS_TOKEN_SECRET);
 
-function searchTweets(sources){
-	var params = {screen_name: 'trend'};
-	client.get('statuses/user_timeline', params, function(error, tweets, response){
-		if (!error) {
-			console.log(tweets);
-		}
+/*********** TRENDS **********************************/
+
+route.use('/trends', function(req, res, next) {
+	//defaults to global trends
+	var location = req.params.location || 1;
+	req.params.query = {id : location};
+	console.log('Trends requested for: ' + location);
+	next();
+});
+
+router.get('/trends/:location', function(req, res, next){
+	var query = req.params.query;
+	if(!query)
+		res.send(400)
+
+	client.get('trends/place', query, function(error, tweets, response){
+		if (!error) 
+			res.send(tweets);
+		else
+			next();
 	});
-}
-
-
-
-function getTopTrends(location){
-
-}
-
-
-router.route('/trends/:location')
-	.get(function(req, res, next){
-		var location = req.params.location ? req.params.location : 1;
-		var params = {id : location};
-
-
-		client.get('trends/place', params, function(error, tweets, response){
-			if (!error) {
-				var ret = JSON.stringify(tweets);
-				res.send(tweets);
-			}
-
-			console.log(error);
-		});
 });
 
 
-router.route('/users')
+/*********** LISTS **********************************/
 
+route.use('/lists', function(req, res,next){
+	//defaults to @BreakingNews\/canada list
+	var list = req.params.list || 6017542;
+	var slug = req.params.list || 'canada'
+	req.params.query = { 
+		list_id : list,
+		slug : slug
+	};
+
+	console.log('List requested for: %s, %s',list, slug);
+	next();
+});
+
+
+route.get('/lists/:id/:slug', function(req, res, next) {
+	var query = req.params.query;
+	if(!query)
+		res.send(400);
+
+	client.get('lists/statuses', query, function(error, tweets, response) {
+		if(!error)
+			res.send(tweets);
+		else
+			next();
+	});
+});
+
+
+
+/*********** Custom Users **********************************/
 
 
 module.exports = router;
