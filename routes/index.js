ec20102f03f1bab2e16f7bfe32ceb3d4b3c51cfe
Adel Lahlou <adellahlou@users.noreply.github.com>
2015-09-28 21:10:51 -0500

    Added search for NYT endpoint

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var router = express.Router();
-
+//var yahooRoutes = require('./yahooRoutes');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.send('Server is up');
 });
 
 router.use('/twitter', require('./twitterRoutes'));
-
+//router.use('/yahoo', );
+router.use('/nyt', require('./nytRoutes'));
 
 module.exports = router;
