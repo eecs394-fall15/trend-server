d937eb3f5eded88c41af587712d8f27511d092e2
Philip Lan <philiplan2017@u.northwestern.edu>
2015-10-01 02:04:09 -0500

    getting rid of cors dependencies, api seems to work fine without

--- app.js
+++ app.js
@@ -5,10 +5,8 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var cors = require('cors');
 var app = express();
 
-app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
