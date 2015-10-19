0394c4a95547440c3ea973257229920389dc8aac
Adel Lahlou <adellahlou@users.noreply.github.com>
2015-10-19 00:03:28 -0500

    Implemented CRU for users, tested CR

--- app.js
+++ app.js
@@ -3,9 +3,19 @@ var path = require('path');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
 var routes = require('./routes/index');
 var app = express();
+
+console.log(process.env.MONGOLAB_URI);
+mongoose.connect(process.env.MONGOLAB_URI);
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function(cb){
+  console.log('Database is now connected.');
+});
+
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
--- routes/users.js
+++ routes/users.js
@@ -3,34 +3,56 @@ var mongoose = require('mongoose');
 var User = require('../lib/models/Users')
 
 
+function cleanTrendPrefs(postStr){
+	if (typeof postStr !== 'string')
+		return {};
 
-router.route('/:userid')
+	var termGroups = postStr.split('|');
+	var ret = {}
+	termGroups.forEach(function(termGroup){
+		termGroup = termGroup.split(':');
+		if(termGroup.length < 2)
+			return;
+
+		var term = termGroup[0]
+		var sites = termGroup[1].split(',');
+		ret[term] = sites;
+	});
+
+	return ret;
+}
+
+router.route('/')
 	//create user
 	.post(function(req, res, next){
 		var b = req.body;
+		var trendPreferences = b.trendPreferences ? cleanTrendPrefs(b.trendPreferences) : {};
 
 		var userParams = {
 			userid : mongoose.Types.ObjectId(),
 			age : b.age || 18,
 			preferences: {
-				trendPreferences : b.trendPreferences || {},
+				trendPreferences : trendPreferences || {},
 				searchHistory : []
 			},
 			subscriptions : b.subscriptions || []
 		};
 
+		console.log(trendPreferences)
 		//save User to db and return status 
 		var newUser = new User(userParams);
 		newUser.save(function(err, newUser){
 			if(!err)
-				res.sendStatus(201);
+				res.status(201).send(newUser);
 			else
 				res.status(400).send(err);
 		});
-	})
+	});
 
+router.route('/:userid')
 	//read user data
 	.get(function(req, res, next){
+		console.log(mongoose.Types.ObjectId());
 		User.findOne( {userid: req.params.userid}, { 
 			//don't send back tons of personal info
 			accounts : 0,
@@ -40,8 +62,12 @@ router.route('/:userid')
 		function foundUser(err, user){
 			if(err)
 				res.status(400).send(err);
-			else if (result) {
+			else if (user) {
+				console.log(user);
 				res.send(user);
+				user.lastLogin = Date.now();
+				user.markModified('lastLogin');
+				user.save();
 			} else 
 				res.status(404).send('User not found');
 		}
@@ -49,6 +75,11 @@ router.route('/:userid')
 
 	//update user object
 	.put(function(req, res, next){
+		var userUpdate  =  req.body.user;
+
+		if(!userUpdate)
+			res.sendStatus(400);
+
 		User.findOne( {userid: req.params.userid}, { 
 			//don't send back tons of personal info
 			accounts : 0,
@@ -61,8 +92,20 @@ router.route('/:userid')
 			else if (!result) {
 				res.status(404).send('User not found');
 			} else {
-				//user.
-				console.log('Make Modify user');
+				var updateKeys = Object.keys(userUpdate);
+				updateKeys.forEach(function(key){
+					if(user[key]) {
+						user[key] = userUpdate[key]
+						user.markModified(key);
+					}
+				});
+
+				user.save(function(err, modifiedUser){
+					if (err)
+						res.status(400).send(err);
+					else
+						res.status(201).send(modifiedUser);
+				})
 			}
 		}
 	})
